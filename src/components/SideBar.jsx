7de0866580a9dd0d42fbc161485efb78bad83d7a
Kelechukwu Ikechukwu <obiefunalelechukwu@gmail.com>
2024-02-15 17:07:48 +0100

    added &apos; to settle eslint error

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ export const SideBar = () => {
   return (
     <div className="p-8">
       <h1 className="text-2xl font-semibold">
-        Hi, Daniel! let's help you get started right away.
+        Hi, Daniel! let&apos;s help you get started right away.
       </h1>
       <p className="text-xs text-gray-500">
         Provide us with a little bit about yourself, this will help us remember
